898c88ce6f75b34f62449ea5442637bf7efc68fc
Nikola Pervic <npervic@itekako.com>
2021-09-19 13:48:55 +0200

    add zip code to Addresses

--- store/models.py
+++ store/models.py
@@ -43,6 +43,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.IntegerField()
 
 
 class Order(models.Model):
