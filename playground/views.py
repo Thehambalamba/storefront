341cf96b52783293a6195077c58061809f0e3114
Nikola Pervic <npervic@itekako.com>
2023-03-26 21:31:42 +0200

    Selecting fields to query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.all()[5:10]
+    query_set = Product.objects.filter(id__in=OrderItem.objects.values_list(
+        'product_id').distinct()).order_by('title')
+
     view_context = {'name': 'Nikola', 'query_set': query_set}
 
     return render(request, 'hello.html', context=view_context)
