9b9756c8b2ffbdb8001263f58da0f3f689a765a6
Nikola Pervic <npervic@itekako.com>
2023-03-26 20:13:48 +0200

    Refrencing fields using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import F
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(
-        Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    query_set = Product.objects.filter(inventory=F('collection__id'))
     view_context = {'name': 'Nikola', 'query_set': list(query_set)}
 
     return render(request, 'hello.html', context=view_context)
