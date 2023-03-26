41016024a65a8841af5a1b99973b6e74766e1384
Nikola Pervic <npervic@itekako.com>
2023-03-26 22:36:05 +0200

    Annotate objects

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from django.db.models.aggregates import Count, Sum, Avg, Min, Max
-from store.models import Order, OrderItem, Product
+from django.db.models import Value, F
+from store.models import Customer
 
 
 def say_hello(request):
-    number_of_orders = Order.objects.aggregate(number_of_orders=Count('id'))
-    number_of_product_1_sold = OrderItem.objects.filter(
-        product__id=1).aggregate(number_of_product_1_sold=Sum('quantity'))
-    number_of_orders_for_customer_1 = Order.objects.filter(
-        customer_id=1).aggregate(number_of_orders_for_customer_1=Count('id'))
-    price_stats_for_products_from_collection_3 = Product.objects.filter(
-        collection__id=3).aggregate(min_price=Min('unit_price'), avg_price=Avg('unit_price'), max_price=Max('unit_price'))
-    result = {**number_of_orders, **number_of_product_1_sold,
-              **number_of_orders_for_customer_1, "price_stats_for_products_from_collection_3": price_stats_for_products_from_collection_3}
+    # add fields to a table using annotate
+    query_set = Customer.objects.annotate(
+        new_id=F('id') + 1, is_new=Value(True))
     view_context = {'name': 'Nikola',
-                    'result': result}
+                    'result': query_set}
 
     return render(request, 'hello.html', context=view_context)
