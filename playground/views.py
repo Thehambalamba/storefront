844e70854d487f92a5fa1e6c426a179f2a7058bf
Nikola Pervic <npervic@itekako.com>
2023-03-26 22:07:40 +0200

    Selecting related objects (exercise)

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Order
 
 
 def say_hello(request):
-    # select_related (one to one)
-    query_set_select_related = Product.objects.select_related(
-        'collection').all()
-    # prefetch_related (many to many)
-    query_set_prefetch_related = Product.objects.prefetch_related(
-        'promotions').all()
-    # combined
-    query_set = Product.objects.prefetch_related(
-        'promotions').select_related('collection').all()
+    # Handling relationships between table with query_sets
+    query_set = Order.objects.select_related(
+        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
 
-    view_context = {'name': 'Nikola', 'query_set': query_set}
+    view_context = {'name': 'Nikola', 'orders': query_set}
 
     return render(request, 'hello.html', context=view_context)
