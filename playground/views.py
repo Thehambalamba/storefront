d0f6641ed785ca4d1bed8c9694b4412c8c007145
Nikola Pervic <npervic@itekako.com>
2023-03-26 20:21:43 +0200

    Sorting data

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.db.models import F
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(inventory=F('collection__id'))
-    view_context = {'name': 'Nikola', 'query_set': list(query_set)}
+    # get first object sorted by unit_price from a query_set
+    product_from_query_set = Product.objects.order_by('unit_price')[0]
+    # gets first object sorted by unit_pricewithout the need of extraction from a query_set
+    product = Product.objects.earliest('unit_price')
+    view_context = {'name': 'Nikola', 'product': product}
 
     return render(request, 'hello.html', context=view_context)
