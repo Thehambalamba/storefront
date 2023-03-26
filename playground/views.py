0f2187d567c71315617a810aa56dd21b1683c977
Nikola Pervic <npervic@itekako.com>
2023-03-26 20:23:26 +0200

    Limiting results (pagination example)

--- playground/views.py
+++ playground/views.py
@@ -3,10 +3,7 @@ from store.models import Product
 
 
 def say_hello(request):
-    # get first object sorted by unit_price from a query_set
-    product_from_query_set = Product.objects.order_by('unit_price')[0]
-    # gets first object sorted by unit_pricewithout the need of extraction from a query_set
-    product = Product.objects.earliest('unit_price')
-    view_context = {'name': 'Nikola', 'product': product}
+    query_set = Product.objects.all()[5:10]
+    view_context = {'name': 'Nikola', 'query_set': query_set}
 
     return render(request, 'hello.html', context=view_context)
