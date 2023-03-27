8934fcc2685517b5035917089d70360ac4b46b7c
Nikola Pervic <npervic@itekako.com>
2023-03-27 21:48:31 +0200

    Querying generic relationships

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
-from django.db.models import Max, Count, Sum, F
-from store.models import Customer, Collection, Product
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    customers_last_order_id = Customer.objects.annotate(
-        last_order_id=Max('order__id')).values()
-    collections_count_of_products = Collection.objects.annotate(
-        count_of_products=Count('product__id')).values()
-    customers_more_than_five_orders = Customer.objects.annotate(
-        orders_count=Count('order')).filter(orders_count__gt=5).values
-    customers_total_amount_spent = Customer.objects.annotate(
-        amount_spent_total=Sum(F('order__orderitem__unit_price') * F('order__orderitem__quantity'))).values()
-    products_top_five_sellers_total_sales = Product.objects.annotate(
-        total_sales=Sum(F('orderitem__unit_price') * F('orderitem__quantity'))).order_by('-total_sales')[:5].values()
+    content_type = ContentType.objects.get_for_model(Product)
+
+    query_set = TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type=content_type,
+            object_id=1
+        )
     view_context = {'name': 'Nikola',
-                    'result': products_top_five_sellers_total_sales}
+                    'result': list(query_set)}
 
     return render(request, 'hello.html', context=view_context)
