b1e3040a2b1d1cf37af6418aede5958b2cee04bc
Nikola Pervic <npervic@itekako.com>
2023-03-28 11:06:53 +0200

    Selecting related objects

--- store/admin.py
+++ store/admin.py
@@ -30,4 +30,9 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+
+
 admin.site.register(models.Collection)
