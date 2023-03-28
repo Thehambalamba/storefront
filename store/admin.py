2649475b1d08f3c6a5ac40f7ee1886e2682e3b38
Nikola Pervic <npervic@itekako.com>
2023-03-28 10:44:00 +0200

    Adding custom models and customizing admin lists

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    ordering = ['title']
+    list_per_page = 10
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
+
+admin.site.register(models.Collection)
