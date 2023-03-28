7b337063229bdf44040742f41f704fa3ba75c46f
Nikola Pervic <npervic@itekako.com>
2023-03-28 12:16:54 +0200

    Using generic relations

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models.aggregates import Count
 from django.db.models import QuerySet
 from django.utils.html import format_html
 from django.utils.http import urlencode
 from django.urls import reverse
+
+from tags.models import TaggedItem
 from . import models
 
 
@@ -21,6 +24,11 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
 
 
+class TagInline(GenericTabularInline):
+    model = TaggedItem
+    autocomplete_fields = ['tag']
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
@@ -28,6 +36,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
+    inlines = [TagInline]
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
