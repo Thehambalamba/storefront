316edfe4c7063178050f4fdbb13bab0131f1e7b9
Nikola Pervic <npervic@itekako.com>
2023-04-01 18:33:24 +0200

    Serializing relationships

--- store/views.py
+++ store/views.py
@@ -9,15 +9,21 @@ from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    query_set = Product.objects.all()
-    serializer = ProductSerializer(query_set, many=True)
+    query_set = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        query_set, many=True, context={'request': request})
 
     return Response(serializer.data)
 
 
 @api_view()
-def product_details(request, id):
+def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
 
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
