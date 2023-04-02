0a8402855c5e3f428926ee21c8be1c2e57df70c1
Nikola Pervic <npervic@itekako.com>
2023-04-02 12:20:57 +0200

    Filtering lists using django-filters

--- store/views.py
+++ store/views.py
@@ -1,24 +1,19 @@
-from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
 
+from .filters import ProductFilter
 from .models import Collection, Product, OrderItem, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get(  # type: ignore
-            'collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
