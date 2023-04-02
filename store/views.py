90f464f20b65beb10043f73f69b408e677651eb7
Nikola Pervic <npervic@itekako.com>
2023-04-02 12:23:29 +0200

    Searching data

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.db.models.aggregates import Count
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .filters import ProductFilter
@@ -12,8 +13,9 @@ from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
 
     def get_serializer_context(self):
         return {'request': self.request}
