2f4b8c8eb2e198da9aab87cd8aa023e2daa6b50b
Nikola Pervic <npervic@itekako.com>
2023-04-02 18:17:49 +0200

    Creating user profiles

--- store/admin.py
+++ store/admin.py
@@ -58,9 +58,11 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['user']
     list_display = ['first_name', 'last_name', 'membership', 'customer_orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     list_per_page = 10
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
