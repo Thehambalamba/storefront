6c4cf5b2676744e070808894a6983fcae6d22fca
Nikola Pervic <npervic@itekako.com>
2023-04-04 23:42:39 +0200

    Applying permisions

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .pagination import DefaultPagination
 from .filters import ProductFilter
@@ -85,6 +86,12 @@ class CustomerViewSet(CreateModelMixin,
                       GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request):
