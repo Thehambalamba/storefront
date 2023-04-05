51b331ed483854950c7a02d07808cb1b4c8f70c8
Nikola Pervic <npervic@itekako.com>
2023-04-05 20:41:40 +0200

    Creating signals

--- core/serializers.py
+++ core/serializers.py
@@ -1,11 +1,10 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
-from rest_framework import serializers
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
-                  'email', 'first_name', 'last_name', 'birth_date']
+                  'email', 'first_name', 'last_name']
 
 
 class UserSerializer(BaseUserSerializer):
--- store/views.py
+++ store/views.py
@@ -92,7 +92,7 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
     @decorators.action(detail=False, methods=['GET', 'PUT'], permission_classes=[permissions.IsAuthenticated])
     def me(self, request):
-        (customer, created) = Customer.objects.get_or_create(
+        customer = Customer.objects.get(
             user_id=request.user.id)
         if request.method == 'GET':
             serializer = CustomerSerializer(customer)
@@ -131,6 +131,6 @@ class OrderViewSet(viewsets.ModelViewSet):
         user = self.request.user
         if user.is_staff:  # type: ignore
             return Order.objects.all()
-        (customer_id, created) = Customer.objects.only(
-            'id').get_or_create(user_id=user.id)  # type: ignore
+        customer_id = Customer.objects.only(
+            'id').get(user_id=user.id)  # type: ignore
         return Order.objects.filter(customer_id=customer_id)
