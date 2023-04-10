a26ca10d0a84019063273d8882469c2004191fa8
Nikola Pervic <npervic@itekako.com>
2023-04-10 22:19:20 +0200

    Write more test for TestCreateCollection

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
 
+from django.contrib.auth.models import User
+
 
 @pytest.mark.django_db
 class TestCreateCollection:
@@ -10,3 +12,26 @@ class TestCreateCollection:
         response = client.post('/store/collections/', {'title': 'a'})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED  # type: ignore
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN  # type: ignore
+
+    def test_if_data_is_invalid_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST  # type: ignore
+        assert response.data['title'] is not None  # type: ignore
+
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED  # type: ignore
+        assert response.data['id'] > 0  # type: ignore
